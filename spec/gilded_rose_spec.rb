d661031ea81b9f114b81e6659e17c200d560d044
Michal Klesinski <michkles@Michals-MacBook-Pro.local>
2016-11-30 14:21:17 +0000

    refactoring tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,12 +2,20 @@ require_relative 'spec_helper'
 
 describe GildedRose do
 
+  let(:foo) { Item.new('foo', 1, 1) }
+  let(:items) { [foo]}
+  let(:gilded_rose) { GildedRose.new(items)}
+
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).not_to eq "fixme"
+      expect(items[0].name).to eq "foo"
+    end
+
+    it "degrades sell-in and quality value" do
+      gilded_rose.update_quality()
+      expect(items[0].sell_in && items[0].quality ).to eq 0
     end
   end
+
 
 end
